f8e91c9edd769b99a769381a15bd3ea3118538ac
Duy Nguyen <ndhduy.7798@gmail.com>
2018-01-09 20:05:31 +0700

    Update transaction model

--- model/transaction.js
+++ model/transaction.js
@@ -3,11 +3,8 @@ var mongoose = require('mongoose');
 var TransactionSchema = new mongoose.Schema(
     {
         src_hash: {type: String},
-        send_address: {type: String, required: true},
-        index: {type: Number},
-        receive_address: {type: String, required: true},
-        amount: {type: Number, required: true},
-        remaining_amount: {type: Number},
+        inputs:[{unlockScript:{type: String}, referencedOutputHash:{type: String},referencedOutputIndex:{type: Number}}],
+          outputs:[{value:{type: Number},lockScript:{type: String}}],
         status: {type: String},
         created_at: {type: String}    
     }
